05feb3e9e95d6a8c1e8cff16883233e237bbd49c
Dekunle013 <Dekunle013@gmail.com>
2024-07-10 08:55:52 +0100

    X Login page

---
+++ src/Login.jsx
@@ -0,0 +1,16 @@
+import './Style.css';
+
+function Login() {
+  return(
+    <div className="container">
+      <div className="left"></div>
+      <div className="right">
+        <h1>Happening now</h1>
+        <h2>Join today.</h2>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
+}
+
+export default  Login;
