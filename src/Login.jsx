13c6b3b09d49e01e0825a4721b63f8d63d112bc1
Dekunle013 <Dekunle013@gmail.com>
2024-07-10 09:30:18 +0100

    Added new features

--- src/Login.jsx
+++ src/Login.jsx
@@ -7,6 +7,15 @@ function Login() {
       <div className="right">
         <h1>Happening now</h1>
         <h2>Join today.</h2>
+
+        <a href="#">Sign up with Google</a>
+        <a href="#">Sign up with Apple</a>
+
+        <div className="hr-cont">
+          <hr />
+          <span>or</span>
+          <hr />
+        </div>
       </div>
       <div className="footer"></div>
     </div>
