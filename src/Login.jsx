a0ad25c993748c31d5c9f8a73bb1fbd874eee570
Dekunle013 <Dekunle013@gmail.com>
2024-07-14 04:21:33 +0100

    Added new features

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,19 +4,31 @@ function Login() {
   return(
     <div className="container">
       <div className="left"></div>
+
       <div className="right">
         <h1>Happening now</h1>
         <h2>Join today.</h2>
 
-        <a href="#">Sign up with Google</a>
-        <a href="#">Sign up with Apple</a>
+        <a className="data-collate" href="#">Sign up with Google</a>
+        <a className="data-collate" href="#">Sign up with Apple</a>
 
         <div className="hr-cont">
           <hr />
           <span>or</span>
           <hr />
         </div>
+
+        <a className="create-link" href="#">Create Account</a>
+
+        <span className="by-sign" id="by-sign">
+          By signing up, you agree to the <a>Terms of Service</a> and <a>Privacy Policy</a>, including <a>Cookie Use</a>.
+        </span>
+
+        <h1>Already have an account?</h1>
+
+        <a href="#">Sign in</a>
       </div>
+
       <div className="footer"></div>
     </div>
   );
